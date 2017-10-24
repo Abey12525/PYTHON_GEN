a41b17d44fe1d7293e96f9b044b2c12f8f3a055c
Abey12525 <abeyrhurtis@cs.ajce.in>
2017-10-24 14:19:57 +0530

    Add files via upload
    knn_logisticRegression_accuracy

--- irisknn.py
+++ irisknn.py
@@ -1,29 +1,55 @@
+from scipy.spatial import distance as d 
+def udistance(t,tr):
+	#returns the distance btw test and train set
+	return d.euclidean(t,tr)
+###############################################
+
+class myKNN():
+	def fit(self,X_train,Y_train):
+		self.x_train=X_train
+		self.y_train=Y_train
+
+
+	def predict(self,X_test):
+		predictions = []
+		for row in X_test:
+			label=self.closest(row)
+			predictions.append(label)
+		return predictions
+
+	
+	def closest(self,row):
+	 	lo_distance=udistance(row,self.x_train[0])
+	 	idx=0
+	 	for i in range(1,len(self.x_train)):
+	 		if(udistance(row,self.x_train[i])<lo_distance):
+	 			lo_distance=udistance(row,self.x_train[i])
+	 			idx=i
+	 	return self.y_train[idx]
+
+###############################################################
+
 import numpy as np
 from sklearn import neighbors, datasets
 
-# import some data to play with
+# import data
 iris = datasets.load_iris()
-X = iris.data # we only take the first two features. 
+X = iris.data  
 Y = iris.target
 
 from sklearn.cross_validation import train_test_split as ts
 x_train,x_test,y_train,y_test=ts(X,Y,test_size=.2)
-print(x_train)
-print("\n")
-print(x_test)
-print("\n")
-print(y_train)
-print("\n")
-print(y_test)
-# step size in the mesh
 
-knn=neighbors.KNeighborsClassifier()
 
-# we create an instance of Neighbours Classifier and fit the data.
-clf=knn.fit(x_train, y_train)
-
+clf=myKNN()
+# we create an instance of myKNN and fit the data.
+clf.fit(x_train,y_train)
 p=clf.predict(x_test)
 
 from sklearn.metrics import accuracy_score
+for i in range(len(p)):
+	print("test set = ",x_test[i,:],end=" ")
+	print("label = ",y_test[i],end=" ")
+	print("prediction =",p[i])
 
 print("Accuracy = ",accuracy_score(y_test,p))
