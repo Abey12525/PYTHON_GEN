584b3acb9531013485373200409bfbe0112c3e2c
Abey12525 <abeyrhurtis@cs.ajce.in>
2017-10-24 14:48:25 +0530

    rfc_commit

--- ranfrst.py
+++ ranfrst.py
@@ -8,7 +8,7 @@ fet=iris.data
 lab=iris.target
 
 from sklearn.cross_validation import train_test_split as ts
-x_train,x_test,y_train,y_test=ts(fet,lab,test_size=.2)
+x_train,x_test,y_train,y_test=rfc(fet,lab,test_size=.2)
 
 clf = rfc()
 clf.fit(x_train,y_train)
